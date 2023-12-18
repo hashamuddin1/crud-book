fcb94f091fb8dedec883a007e8420426d182ee95
hashamuddin1 <73032957+hashamuddin1@users.noreply.github.com>
2023-12-18 21:56:39 +0500

    learn custom pipes

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -10,6 +10,7 @@ import {
 } from '@nestjs/common';
 import { BookService } from './book.service';
 import { Book } from './data/book.dto';
+import { bookPipe } from './pipes/book.pipe';
 
 @Controller('book')
 export class BookController {
@@ -34,16 +35,23 @@ export class BookController {
   addBook(@Body() book: Book): string {
     return this.BookService.addBookService(book);
   }
-  
+
   @Get('/findBook/:id')
   findBook(@Param('id') id: number): string {
     console.log(id, typeof id);
     return 'fetch book By ID';
   }
 
+  //pipe
   @Get('/:id')
   findBookById(@Param('id', ParseIntPipe) id: number): string {
     console.log(id, typeof id);
     return 'fetch book By ID';
+  }
+
+  //custom pipe
+  @Post("/add")
+  insertBook(@Body(new bookPipe()) book:Book):string{
+    return "insert book"
   }
 }
---
+++ src/book/pipes/book.pipe.ts
@@ -0,0 +1,12 @@
+import {
+  PipeTransform,
+  ArgumentMetadata,
+  BadRequestException,
+} from '@nestjs/common';
+
+export class bookPipe implements PipeTransform {
+  transform(value: any, metadata: ArgumentMetadata): any {
+    if (value.id === 1) return value;
+    else throw new BadRequestException('Validation Failed');
+  }
+}
