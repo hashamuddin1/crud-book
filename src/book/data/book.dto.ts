3496fc36cf049e9d35db0b482e9f1c6398a348dd
hashamuddin1 <73032957+hashamuddin1@users.noreply.github.com>
2023-12-20 22:30:11 +0500

    start class validation

--- src/book/data/book.dto.ts
+++ src/book/data/book.dto.ts
@@ -1,6 +1,15 @@
-export interface Book {
+import { IsString } from 'class-validator';
+
+export class Book {
+  @IsString()
   id: string;
+
+  @IsString()
   title: string;
+
+  @IsString()
   author: string;
+
+  @IsString()
   published: string;
 }
