4c1bc45706ec2c80a867662cfe5249e6e7a5e5f8
hashamuddin1 <73032957+hashamuddin1@users.noreply.github.com>
2023-12-20 22:43:33 +0500

    revert validation pipe

--- src/book/pipes/book.pipe.ts
+++ src/book/pipes/book.pipe.ts
@@ -3,15 +3,9 @@ import {
   ArgumentMetadata,
   BadRequestException,
 } from '@nestjs/common';
-import { Book } from '../data/book.dto';
-import { plainToInstance } from 'class-transformer';
 
 export class bookPipe implements PipeTransform {
   transform(value: any, metadata: ArgumentMetadata): any {
-    //Class transformer convert obj to class
-    const bookClass = plainToInstance(Book, value);
-
-    //apply class validation
     if (value.id === 1) return value;
     else throw new BadRequestException('Validation Failed');
   }
